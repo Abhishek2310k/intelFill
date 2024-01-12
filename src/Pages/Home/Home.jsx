ccf1536f96e30c0a2a980bac986588e8869e1e3f
Abhishek2310k <abhishekku@iitbhilai.ac.in>
2024-01-12 11:54:58 +0530

    initial CSS

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Gallery from '../../components/Image_gallery/Gallery'
+const Home = () => {
+  return (
+    <div className='home'>
+      <Gallery/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { CiSearch } from "react-icons/ci";
+import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
+import { TextField } from '@mui/material';
+// import SearchIcon from '@mui/icons-material/Search';
+import './Navbar.scss';
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='wrapper'>
+      <div className='left'>
+        <img className='logo' src='../../images/logo.png' alt=""/>
+        <div className='item'>
+          <span>Home</span>
+        </div>
+        <div className='item'>
+          <span>Results</span>
+        </div>
+        <div className='item'>
+          <span>FAQ</span>
+        </div>
+        <div className='item'>
+          <span>About Us</span>
+        </div>
+        <div className='item'>
+          <span>Contact Us</span>
+        </div>
+        <div className="item search">
+          <form action="#">
+          <CiSearch color='gray'/>
+          <input type="text" name="search" placeholder='Search exams here'/>
+          </form>
+        </div>
+        <div className='item'>
+          <NotificationsActiveOutlinedIcon color='primary'/>
+        </div>
+        <div className='item'>
+          <div className='profile'>
+            <img src='../../images/profile_img.jpg' alt=''/>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
